3f797b00c23f3a71e113823f40167f294a8b79e2
ad-world <67097683+ad-world@users.noreply.github.com>
2023-08-20 12:59:40 -0400

    8.2 Robot In Grid (+ isReachable extension using bottom-up dp)

--- src/main/java/DynamicProgramming.java
+++ src/main/java/DynamicProgramming.java
@@ -1,4 +1,7 @@
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class DynamicProgramming {
     // 8.1 Triple Step
@@ -22,5 +25,58 @@ public class DynamicProgramming {
                 + tripleStep(numStep - 2, dp)
                 + tripleStep(numStep - 3, dp);
         return dp[numStep];
+    }
+
+    // 8.2 Robots in a grid
+    public ArrayList<Point> robotInGrid(boolean[][] grid) {
+        // assume tha robot can move into cell with value 0, but cannot move into cell with value 1
+        ArrayList<Point> paths = new ArrayList<>();
+        HashSet<Point> failed = new HashSet<>();
+
+        if(getPath(grid, grid.length - 1, grid[0].length - 1, paths, failed)) {
+            return paths;
+        }
+
+        return null;
+    }
+
+    private boolean getPath(boolean[][] grid, int row, int col, ArrayList<Point> paths, HashSet<Point> failedPaths) {
+        if(row < 0 || col < 0 || !grid[row][col]) return false;
+        Point p = new Point(row, col);
+
+        if(failedPaths.contains(p)) return false;
+
+        boolean isOrigin = row == 0 && col == 0;
+        if(isOrigin
+                || getPath(grid, row - 1, col, paths, failedPaths)
+                || getPath(grid, row, col - 1, paths, failedPaths)) {
+            paths.add(p);
+            return true;
+        }
+
+        failedPaths.add(p);
+        return false;
+    }
+
+    public boolean isReachable(boolean[][] grid) {
+        if(grid.length == 0 || grid[0].length == 0) return false;
+        boolean[][] dp = new boolean[grid.length][grid[0].length];
+
+        for(boolean[] row: dp) {
+            Arrays.fill(row, false);
+        }
+
+        dp[0][0] = true;
+        dp[0][1] = grid[0][1];
+        dp[1][0] = grid[1][0];
+        for(int i = 1; i < grid.length; i++) {
+            for(int j = 1; j < grid[0].length; j++) {
+                if(grid[i][j]) {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                }
+            }
+        }
+
+        return dp[grid.length - 1][grid[0].length - 1];
     }
 }
--- src/test/java/DynamicProgrammingTest.java
+++ src/test/java/DynamicProgrammingTest.java
@@ -15,4 +15,29 @@ public class DynamicProgrammingTest {
         assertEquals(dp.tripleStep(4), 7);
         assertEquals(dp.tripleStep(5), 13);
     }
+
+    @Test
+    public void isReachableTest() {
+        DynamicProgramming dp = new DynamicProgramming();
+
+        boolean[][] arr1 = new boolean[][]{
+                {true, true, false, false},
+                {true, true, false, false},
+                {false, false, false, false},
+                {false, false, false, false}
+        };
+
+        assertFalse(dp.isReachable(arr1));
+
+        boolean[][] arr2 = new boolean[][]{
+                {true, true, false, false},
+                {false, true, false, false},
+                {false, true, true, false},
+                {false, false, true, true}
+        };
+
+        assertTrue(dp.isReachable(arr2));
+
+    }
+
 }
